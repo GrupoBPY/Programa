812c5e9033d16375f2c7688112f300523cdbd3d2
Brenda <Brenda@DESKTOP-UR7EQQC>
2024-09-10 22:54:27 -0300

    Refiz TUDO.

---
+++ SoftwareAssistenciaTecnica-BRENDA472/src/main/java/com/mycompany/softwareassistenciatecnica/brenda472/SoftwareAssistenciaTecnicaBRENDA472.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.softwareassistenciatecnica.brenda472;
+
+/**
+ *
+ * @author Brenda
+ */
+public class SoftwareAssistenciaTecnicaBRENDA472 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
